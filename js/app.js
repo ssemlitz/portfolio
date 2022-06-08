2d68cd78212042cd7cd6586c5f3edfbe08eac3fa
ssemlitz <sdsemlitz@yahoo.com>
2022-06-08 09:02:14 -0400

    working my-work cards

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@
 /*------------------------ Cached Element References ------------------------*/
 
 const h1 = document.querySelector('h1')
+const cardContainer = document.getElementById('card-container')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -21,3 +22,28 @@ function h1TextBounce() {
 function h1RemoveTextBounce() {
   h1.classList.remove('animate__bounce')
 }
+
+
+
+import { projectData } from "./project-data.js"
+
+
+let projectMarkup = projectData.map(project =>
+  `
+    <div class="card text-center" style="width: 18rem;">
+      <img src="${project.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${project.title}</h5>
+        <p class="card-text">${project.description}</p>
+        <div>
+          <a href="${project.github}" class="btn btn-primary">GitHub</a>
+          <a href="${project.deployment}" class="btn btn-secondary">Deployment</a>
+        </div>
+      </div>
+    </div>
+  `  
+).join('')
+
+cardContainer.innerHTML = projectMarkup
+
+console.log('#work-h2')
