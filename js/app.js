8c3dbabce4993f2f47977a39f9b75e88cbf9aeec
ssemlitz <sdsemlitz@yahoo.com>
2022-06-08 16:37:59 -0400

    working light-dark mode

--- js/app.js
+++ js/app.js
@@ -33,10 +33,6 @@ function toggleLightDark() {
   body.className = body.className === "dark" ? "" : "dark"
 }
 
-function toggleLightDark() {
-  body.className = body.className === "dark" ? "" : "dark"
-}
-
 function checkDarkPref() {
   if (
     window.matchMedia("(prefers-color-scheme:dark)").matches &&
