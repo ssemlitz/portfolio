be2601c838cf38a73be1fb0cd9d54f6105f59729
ssemlitz <sdsemlitz@yahoo.com>
2022-08-22 09:54:14 -0400

    change resume and project data

--- js/project-data.js
+++ js/project-data.js
@@ -14,13 +14,6 @@ let projectData = [
     description: 'A zombie dating app built with the MERN stack.'
   },
   {
-    title: 'Tic Tac Toe',
-    deployment: 'https://ssemlitz-tic-tac-toe.netlify.app/',
-    image: '/assets/images/tic-tac-toe.png',
-    github: 'https://github.com/ssemlitz/ttt-weekend',
-    description: 'A web browser game of tic-tac-toe with Dogs vs Cats.'
-  },
-  {
     title: 'RT-Deals',
     deployment: 'https://rt-deals.herokuapp.com/deals',
     image: './assets/images/rt-deals-landing.png',
@@ -28,18 +21,11 @@ let projectData = [
     description: "A deal aggregator application created using the MEN stack."
   },
   {
-    title: 'Flashcards',
+    title: 'Tic Tac Toe',
     deployment: 'https://ssemlitz-tic-tac-toe.netlify.app/',
-    image: './assets/images/python-flashcard-project.png',
-    github: '',
-    description: "A python project designed to make flashcards and then create a new text file of the words you don't know."
-  },
-  {
-    title: 'Snake',
-    deployment: 'https://ssemlitz-tic-tac-toe.netlify.app/',
-    image: './assets/images/python-snake.png',
-    github: '',
-    description: "A python project designed with OOP in mind to create the classic game Snake."
+    image: '/assets/images/tic-tac-toe.png',
+    github: 'https://github.com/ssemlitz/ttt-weekend',
+    description: 'A web browser game of tic-tac-toe with Dogs vs Cats.'
   }
 ]
 
